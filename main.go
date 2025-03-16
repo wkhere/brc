f96ca60a01a60ca96ff73f2835349f3afc9be1a8
Wojciech Kaczmarek <wojtekk@kofeina.net>
2025-03-16 02:03:24 +0100

    file args

--- main.go
+++ main.go
@@ -9,9 +9,14 @@ import (
 	"github.com/andybalholm/brotli"
 )
 
+const fileExt = ".br"
+
 type action struct {
+	fileIn        string
+	fileOut       string
 	compress      bool
 	compressLevel int
+	force         bool
 
 	help func()
 }
@@ -21,11 +26,43 @@ var defaultAction = action{
 	compressLevel: 6,
 }
 
-func run(a action) error {
+func openIn(path string) (*os.File, error) {
+	if path == "-" {
+		return os.Stdin, nil
+	}
+	return os.Open(path)
+}
+
+func openOut(path string, force bool) (*os.File, error) {
+	if path == "-" {
+		return os.Stdout, nil
+	}
+	var wflag int
+	if force {
+		wflag = os.O_TRUNC
+	} else {
+		wflag = os.O_EXCL
+	}
+	return os.OpenFile(path, os.O_WRONLY|os.O_CREATE|wflag, 0644)
+}
+
+func run(a action) (err error) {
+	in, err := openIn(a.fileIn)
+	if err != nil {
+		return err
+	}
+	defer in.Close()
+
+	out, err := openOut(a.fileOut, a.force)
+	if err != nil {
+		return err
+	}
+	defer safeClose(out, &err)
+
 	switch {
 	case a.compress:
-		w := brotli.NewWriterLevel(os.Stdout, a.compressLevel)
-		_, err := io.Copy(w, os.Stdin)
+		w := brotli.NewWriterLevel(out, a.compressLevel)
+		_, err = io.Copy(w, in)
 		if err != nil {
 			return fmt.Errorf("compress: %w", err)
 		}
@@ -35,8 +72,8 @@ func run(a action) error {
 		}
 
 	case !a.compress:
-		r := brotli.NewReader(os.Stdin)
-		_, err := io.Copy(os.Stdout, r)
+		r := brotli.NewReader(in)
+		_, err := io.Copy(out, r)
 		if err != nil {
 			return fmt.Errorf("decompress: %w", err)
 		}
@@ -58,6 +95,13 @@ func main() {
 	err = run(a)
 	if err != nil {
 		die(1, err)
+	}
+}
+
+func safeClose(f *os.File, errp *error) {
+	cerr := f.Close()
+	if cerr != nil && *errp == nil {
+		*errp = cerr
 	}
 }
 
